a32e456d859b939bac576a233e3a90b2e498ea39
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-23 15:40:11 +0530

    React_Fragments

--- App.js
+++ App.js
@@ -13,24 +13,78 @@
  * Consistent Hashing Algorithm
  * Zero Config
  * Tree Shaking-removing unwanted code
+ *
  * Transitive Dependancies
  */
-import { createElement } from "react";
+
+/**
+ * Header
+ *  -Logo
+ *  -Nav Items(Right Side)
+ *  -Cart
+ *
+ * Body
+ *  -Search Bar
+ *  -Restrauntlist
+ *  -Restaurant Card
+ *  -Image
+ *  -Name
+ *  -Rating
+ *  -Cusines
+ *
+ *
+ *
+ * Footer
+ *  -Links
+ *  -Copyright
+ *
+ */
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Heading = () => (
-  <h1 id="title" key="h2">
-    Food Villa
-  </h1>
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      src="https://i.playboard.app/p/AAUvwnid3BDOuAtsI4uGV5D5H4LGGfPuPOQD-w88CHLP/default.jpg"
+      alt="logo"
+    />
+  </a>
 );
 const heading2 = <h1>Oreo</h1>;
 
-const HeaderComponent = () => (
-  <div>
-    <Heading></Heading>
-    <h1>Hue </h1>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root1"));
-root.render(<HeaderComponent />);
+const Body = () => {
+  return <h4>Body</h4>;
+};
+
+const Footer = () => {
+  return <h5>Footer</h5>;
+};
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<Header />);
+root.render(<AppLayout></AppLayout>);
