27e46d9c5e50b4aa99f6d4b61e263bb7ce423233
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-23 22:20:28 +0530

    segregating_components

---
+++ src/Body.js
@@ -0,0 +1,21 @@
+import { restaurantList } from "./constants";
+import { RestaurantCard } from "./RestrauntCard";
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      {/* {RestrauntCard()} */}
+      {/* <RestrauntCard restaurant={restaurantList[0]} />
+        <RestrauntCard restaurant={restaurantList[1]} />
+        <RestrauntCard restaurant={restaurantList[2]} />
+        <RestrauntCard restaurant={restaurantList[3]} />
+        <RestrauntCard restaurant={restaurantList[4]} />
+        <RestrauntCard restaurant={restaurantList[5]} /> */}
+
+      {restaurantList.map((restaurant) => {
+        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
+      })}
+    </div>
+  );
+};
+
+export default Body;
