ce7db13b39c68dfe60b95672c391487dcba3375a
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-24 18:14:02 +0530

    loading_swiggy-api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import { restaurantList } from "../constants";
 import { RestaurantCard } from "./RestrauntCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { swiggy_api_URL } from "../constants";
 
 function filterData(searchText, restaurants) {
   const filterData = restaurants.filter((restaurant) =>
@@ -23,6 +24,20 @@ const Body = () => {
 
   //searchClick is just used to check if the button has been clicked
   const [searchClicked, setSearchClicked] = useState("false");
+
+  //useEffect is being used, if the useEffect is called with an empty depandancy array, the useEffect will be called just once and that too after the intial render
+
+  // useEffect(() => {
+  //   getRestaurants();
+  // }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(swiggy_api_URL);
+    const json = await data.json;
+    //Optional Chaining is a better way
+    setRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
   return (
     <>
       <div className="search-container">
@@ -48,16 +63,7 @@ const Body = () => {
       >
         Search
       </button>
-      <h1>{searchClicked}</h1>
       <div className="restaurant-list">
-        {/* {RestrauntCard()} */}
-        {/* <RestrauntCard restaurant={restaurantList[0]} />
-        <RestrauntCard restaurant={restaurantList[1]} />
-        <RestrauntCard restaurant={restaurantList[2]} />
-        <RestrauntCard restaurant={restaurantList[3]} />
-        <RestrauntCard restaurant={restaurantList[4]} />
-        <RestrauntCard restaurant={restaurantList[5]} /> */}
-
         {restaurants.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const Title = () => (
   <a href="/">
     <img
