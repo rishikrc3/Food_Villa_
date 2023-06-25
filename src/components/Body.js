404cef19cb1fdde18aeb0cb5e43135565cd43f7f
Rishik Chakravarty <rishikrc3@gmail.com>
2023-06-25 16:42:00 +0530

    improving

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,13 +17,8 @@ const Body = () => {
 
   //in order to filter the restaurants, we use a local state variable to filter the data
   const [restaurants, setRestaurants] = useState(restaurantList);
-
-  //searchVar is used to update the search bar
-  const serachVar = useState();
-  [searchText, setSearchText] = serachVar;
-
-  //searchClick is just used to check if the button has been clicked
-  const [searchClicked, setSearchClicked] = useState("false");
+  const [searchText, setSearchText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   //useEffect is being used, if the useEffect is called with an empty depandancy array, the useEffect will be called just once and that too after the intial render
 
@@ -37,10 +32,25 @@ const Body = () => {
     //Optional Chaining is a better way
     setRestaurants(json?.data?.cards[2]?.data?.data?.cards);
   }
+  const searchData = (searchText, restaurants) => {
+    if (searchText !== "") {
+      const data = filterData(searchText, restaurants);
+      setRestaurants(data);
+      setErrorMessage("");
+      if (data.length === 0) {
+        setErrorMessage("No matches restaurant found");
+      }
+    } else {
+      setErrorMessage("");
+      setRestaurants(restaurants);
+    }
+  };
 
+  // if allRestaurants is empty don't render restaurants cards
+  if (!restaurants) return null;
   return (
     <>
-      <div className="search-container">
+      {/* <div className="search-container">
         <input
           type="text"
           className="search-input"
@@ -62,7 +72,28 @@ const Body = () => {
         }}
       >
         Search
-      </button>
+        
+      </button> */}
+
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search a restaurant you want..."
+          value={searchText}
+          // update the state variable searchText when we typing in input box
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            // user click on button searchData function is called
+            searchData(searchText, restaurants);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="restaurant-list">
         {restaurants.map((restaurant) => {
           return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,28 @@ const Title = () => (
   </a>
 );
 
+// const Header = () => {
+//   return (
+//     <div className="header">
+//       <Title />
+//       <div className="nav-items">
+//         <ul>
+//           <li>Home</li>
+//           <li>About</li>
+//           <li>Contact</li>
+//           <li>Cart</li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
 const Header = () => {
+  // use useState for user logged in or logged out
+  const [isLoggedin, setIsLoggedin] = useState(true);
+
   return (
     <div className="header">
       <Title />
@@ -18,7 +39,24 @@ const Header = () => {
           <li>Home</li>
           <li>About</li>
           <li>Contact</li>
-          <li>Cart</li>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
+          <li>
+            {/* use conditional rendering for login and logout */}
+            {isLoggedin ? (
+              <button
+                className="logout-btn"
+                onClick={() => setIsLoggedin(false)}
+              >
+                Logout
+              </button>
+            ) : (
+              <button className="login-btn" onClick={() => setIsLoggedin(true)}>
+                Login
+              </button>
+            )}
+          </li>
         </ul>
       </div>
     </div>
